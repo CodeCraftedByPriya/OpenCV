30283d2c98e39482380caf36c7153a5e1aaf5e09
Priyalakshmi <sppriya1602.06@gmail.com>
2024-07-07 00:21:07 +0530

    Update 3_colour_change.py

--- 3_colour_change.py
+++ 3_colour_change.py
@@ -10,3 +10,16 @@ cv2.imshow('Gray Scale Image', gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# Multiple modes - now you can do it without the multiple lines code above.
+'''To change the image color to gray add -- 0
+   To change the image color to original -- 1'''
+img = cv2.imread(image_path, 0)
+cv2.imshow("Ori Img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Change gray to colour
+gray = cv2.imread(image_path, 1)
+cv2.imshow("Gray to Ori Img", gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
