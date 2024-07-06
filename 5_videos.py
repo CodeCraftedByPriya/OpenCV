9fa2f997248975fa36ed54df71b26af3ac22ed6d
Priyalakshmi <sppriya1602.06@gmail.com>
2024-07-07 02:08:27 +0530

    Update 5_videos.py

--- 5_videos.py
+++ 5_videos.py
@@ -1,1 +1,25 @@
+import cv2
+
+# Capture Video
+# Using Webcam
+abc = cv2.VideoCapture(0)
+while(True):
+    ret_, frame = abc.read()                                      # Reads each frame (25 per sec)
+    cv2.imshow("abc", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):                         # Here this 'q' is the exit key.
+        break
+        abc.release()
+cv2.destroyAllWindows()
+
+
 # Capture Videos using OpenCV
+if (abc.isOpen() == False):
+    print("Could not open the camera")
+
+# Set Resolution
+width = int(abc.get(3))
+height = int(abc.get(4))
+
+# video codded - encoding and decoding
+video_cod = cv2.VideoWriter_fourcc(*'XVID')
+video_output = cv2.VideoWriter
